509cd95c35bd88a74f131d5240610d4e4d817265
newtype51 <connor.holmes@mail.utoronto.ca>
2024-05-24 13:12:10 -0400

    minor change to run stats for new param tuning.

--- get_run_stats.py
+++ get_run_stats.py
@@ -347,9 +347,10 @@ def rmse(outputs_se3, targets_se3, filter=False):
 def print_tables_RSS():
     results_paths = [
         "results/test_svd_v2/inthedark",
+        "results/test_svd_v3/inthedark",
         "results/test_sdpr_v3/inthedark",
     ]
-    labels = ["SVD", "SDPR"]
+    labels = ["SVD", "VGG16 SVD", "VGG16 SDPR"]
     home = "/home/cho/projects/deep_learned_visual_features"
     map_ids = [2, 11, 16, 17, 23, 28]
     compare_stats = CompareStats(
